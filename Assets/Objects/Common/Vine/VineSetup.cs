e43b9223dd3a4e392bda7710dff5ddf01ef101ca
Jack Atherton <atherton.lawrence@gmail.com>
2018-08-13 13:07:24 -0700

    cleaner vine prefab

---
+++ Assets/Objects/Common/Vine/VineSetup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VineSetup : MonoBehaviour
+{
+    public Transform headToTrack, armToTrack1, armToTrack2;
+    public VineEndJointController myHeadJoint, myArmJoint1, myArmJoint2;
+
+    // Use this for initialization
+    void Awake()
+    {
+        myHeadJoint.myThingToFollow = headToTrack;
+        myArmJoint1.myThingToFollow = armToTrack1;
+        myArmJoint2.myThingToFollow = armToTrack2;
+    }
+}
