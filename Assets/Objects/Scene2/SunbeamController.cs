fd7535443d14186c09114f2370ee7823b70d511d
Jack Atherton <atherton.lawrence@gmail.com>
2018-08-14 11:43:01 -0700

    rotating and fading in and out sunbeams

---
+++ Assets/Objects/Scene2/SunbeamController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunbeamController : MonoBehaviour
+{
+    public Transform visualPart;
+    public float fadeCycleSeconds = 10;
+    private Color startColor;
+    private float maxAlpha;
+    private MeshRenderer visualRenderer;
+
+    private void Start()
+    {
+        visualRenderer = visualPart.GetComponent<MeshRenderer>();
+        startColor = visualRenderer.material.color;
+        maxAlpha = startColor.a;
+        Debug.Log( maxAlpha );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        visualPart.rotation *= Quaternion.AngleAxis( 10 * Time.deltaTime, Vector3.up );
+
+
+        // color + glossiness tone down to 0 to make invisible
+        float currentOpacity = 0.5f * ( 1 - Mathf.Cos( 2 * Mathf.PI * Time.time / fadeCycleSeconds ) );
+        Color newColor = new Color( startColor.r, startColor.g, startColor.b, currentOpacity * maxAlpha );
+        visualRenderer.material.color = newColor;
+        visualRenderer.material.SetFloat( "_Glossiness", currentOpacity );
+    }
+}
