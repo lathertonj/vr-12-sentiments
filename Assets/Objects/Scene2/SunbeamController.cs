8b9a3efbbb57292dead8488ea985ae10c24862b6
Jack Atherton <atherton.lawrence@gmail.com>
2018-08-14 13:55:59 -0700

    move sunbeams randomly and use icosahedron instead of cube

--- Assets/Objects/Scene2/SunbeamController.cs
+++ Assets/Objects/Scene2/SunbeamController.cs
@@ -6,28 +6,68 @@ public class SunbeamController : MonoBehaviour
 {
     public Transform visualPart;
     public float fadeCycleSeconds = 10;
+    public float maxBaseAngle = 25;
+    public float maxSweepAngle = 8;
+    public float maxRepositionRadius = 3f;
     private Color startColor;
     private float maxAlpha;
     private MeshRenderer visualRenderer;
+    private Vector3 originalPos;
+    private float timePhase;
 
     private void Start()
     {
         visualRenderer = visualPart.GetComponent<MeshRenderer>();
         startColor = visualRenderer.material.color;
         maxAlpha = startColor.a;
-        Debug.Log( maxAlpha );
+        originalPos = transform.position;
+        
+        timePhase = Random.Range( 1f, fadeCycleSeconds - 1f );
+        InvokeRepeating( "SwitchLocation", timePhase, fadeCycleSeconds );
+        visualRenderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // rotate the beam internally
         visualPart.rotation *= Quaternion.AngleAxis( 10 * Time.deltaTime, Vector3.up );
 
+        // rotate the beam externally
+        transform.rotation *= Quaternion.AngleAxis( rotationPerSecond.x * Time.deltaTime, Vector3.left ) * Quaternion.AngleAxis( rotationPerSecond.z * Time.deltaTime, Vector3.forward );
 
-        // color + glossiness tone down to 0 to make invisible
-        float currentOpacity = 0.5f * ( 1 - Mathf.Cos( 2 * Mathf.PI * Time.time / fadeCycleSeconds ) );
-        Color newColor = new Color( startColor.r, startColor.g, startColor.b, currentOpacity * maxAlpha );
-        visualRenderer.material.color = newColor;
-        visualRenderer.material.SetFloat( "_Glossiness", currentOpacity );
+        if( Time.time > timePhase )
+        {
+            // color tone down to 0 to make invisible
+            // (subtract a little and clamp01 so it's 0 for longer)
+            float currentOpacity = Mathf.Clamp01( 0.5f * ( 1 - Mathf.Cos( 2 * Mathf.PI * ( Time.time - timePhase ) / fadeCycleSeconds ) ) - 0.001f );
+            Color newColor = new Color( startColor.r, startColor.g, startColor.b, currentOpacity * maxAlpha );
+            visualRenderer.material.color = newColor;
+        }
+    }
+
+    Vector3 rotationPerSecond = Vector3.zero;
+
+    // called every time the sunbeam moves
+    void SwitchLocation()
+    {
+        // new angle 
+        float newXRotation = Random.Range( -maxBaseAngle, maxBaseAngle );
+        float newZRotation = Random.Range( -maxBaseAngle, maxBaseAngle );
+        transform.rotation = Quaternion.AngleAxis( newXRotation, Vector3.left ) * Quaternion.AngleAxis( newZRotation, Vector3.forward );
+
+        // new slow rotation
+        rotationPerSecond.x = Random.Range( -maxSweepAngle, maxSweepAngle ) / fadeCycleSeconds;
+        rotationPerSecond.z = Random.Range( -maxSweepAngle, maxSweepAngle ) / fadeCycleSeconds;
+
+        // new position
+        transform.position = originalPos + new Vector3(
+            Random.Range( -maxRepositionRadius, maxRepositionRadius ),
+            0,
+            Random.Range( -maxRepositionRadius, maxRepositionRadius )
+        );
+
+        // enable in case disabled
+        visualRenderer.enabled = true;
     }
 }
