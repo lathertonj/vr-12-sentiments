62405950745ab2ae6ef7e33a65688ef18b06f89b
Jack Atherton <atherton.lawrence@gmail.com>
2018-08-15 15:42:16 -0700

    first version of vine growth in scene 1

---
+++ Assets/Objects/Scene1/VineGrowth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VineGrowth : MonoBehaviour
+{
+    public ControllerAccessors leftController, rightController;
+    public Transform vrRoom;
+    private Vector3 previousPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        previousPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float heightChange = transform.position.y - previousPosition.y;
+        if( heightChange > 0 && ( leftController.IsSqueezed() || rightController.IsSqueezed() ) )
+        {
+            ushort intensity = (ushort) heightChange.MapClamp( 0, 0.01f, 0, 3999 );
+            leftController.Vibrate( intensity );
+            rightController.Vibrate( intensity );
+
+            // change my scale
+            float scaleMultiplier = heightChange.MapClamp( 0, 0.01f, 1, 1.01f );
+            vrRoom.localScale *= scaleMultiplier;
+        }
+
+        previousPosition = transform.position;
+    }
+}
