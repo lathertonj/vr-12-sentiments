d5b3a4941195681564cefad767d215119b9abc51
Jack Atherton <atherton.lawrence@gmail.com>
2018-10-10 15:07:43 -0700

    basically broken but implemented seedlings raising during second half chords

--- Assets/Objects/Common/Seedling/ApplyWindToSeedlings2.cs
+++ Assets/Objects/Common/Seedling/ApplyWindToSeedlings2.cs
@@ -11,6 +11,7 @@ public class ApplyWindToSeedlings2 : MonoBehaviour
 
     public ChuckSubInstance myChuck;
     private ChuckFloatSyncer myIntensitySyncer;
+    private ChuckEventListener mySecondHalfListener;
 
     // Use this for initialization
     void Start()
@@ -20,6 +21,9 @@ public class ApplyWindToSeedlings2 : MonoBehaviour
 
         myIntensitySyncer = gameObject.AddComponent<ChuckFloatSyncer>();
         myIntensitySyncer.SyncFloat( myChuck, "scene6SwellIntensity" );
+
+        mySecondHalfListener = gameObject.AddComponent<ChuckEventListener>();
+        mySecondHalfListener.ListenForEvent( myChuck, "ahhChordChange", AdvanceToSecondHalf );
 
         seedlings = GetComponentsInChildren<Rigidbody>();
         currentWindDirection = Vector3.right;
@@ -33,14 +37,42 @@ public class ApplyWindToSeedlings2 : MonoBehaviour
         ) * currentWindDirection;
     }
 
+    private bool inSecondHalf = false;
+    private void AdvanceToSecondHalf()
+    {
+        inSecondHalf = true;
+        // only listen to the first one
+        mySecondHalfListener.StopListening();
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         float currentWindIntensity = maxWindIntensity * 0.0005f * myIntensitySyncer.GetCurrentValue();
+        
+        Vector3 forceDirection = Vector3.zero;
+        Vector3 forceTorque = Vector3.zero;
+        // in second half, mostly go up; otherwise, go sideways
+        if( inSecondHalf )
+        {
+            forceDirection = currentWindIntensity * Random.Range( 0.8f, 1.8f ) * Vector3.up;
+            forceDirection += currentWindDirection * 0.01f * currentWindIntensity;
+            forceTorque = new Vector3(
+                Random.Range( -1f, 1f ),
+                Random.Range( -1f, 1f ),
+                Random.Range( -1f, 1f )
+            );
+        }
+        else
+        {
+            forceDirection = currentWindDirection * currentWindIntensity;
+        }
 
+        // apply the force
         foreach( Rigidbody seedling in seedlings )
         {
-            seedling.AddForce( currentWindDirection * currentWindIntensity );
+            seedling.AddForce( forceDirection );
+            //seedling.AddTorque( forceTorque );
         }
     }
 }
