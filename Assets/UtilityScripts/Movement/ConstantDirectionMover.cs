6d2665974f151b3738d722ae7cf5ad92802da7e3
Jack Atherton <atherton.lawrence@gmail.com>
2018-10-24 13:04:14 -0700

    satisfying method of movement for mvmt 7

---
+++ Assets/UtilityScripts/Movement/ConstantDirectionMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantDirectionMover : MonoBehaviour
+{
+
+    private Vector3 currentDirection;
+    private float currentSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        currentDirection = Vector3.zero;
+        currentSpeed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position += Time.deltaTime * currentSpeed * currentDirection;
+    }
+
+	public void SetDirection( Vector3 direction, float speed )
+	{
+		currentDirection = direction;
+		currentSpeed = speed;
+	}
+}
---
+++ Assets/UtilityScripts/Movement/ControllerDirectionMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerDirectionMovement : MonoBehaviour
+{
+
+    ControllerAccessors controller;
+    ConstantDirectionMover room;
+	public float maxSpeed = 3f;
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<ControllerAccessors>();
+        room = GetComponentInParent<ConstantDirectionMover>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( controller.IsUnSqueezed() )
+        {
+            Vector3 v = controller.Velocity();
+            room.SetDirection( v.normalized, v.magnitude.MapClamp( 0, 4, 0, maxSpeed ) );
+        }
+
+    }
+}
