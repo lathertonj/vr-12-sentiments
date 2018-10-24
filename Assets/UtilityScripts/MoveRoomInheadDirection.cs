097cc8f00197898532f7a7501cb1c1ccd207281d
Jack Atherton <atherton.lawrence@gmail.com>
2018-10-24 11:56:20 -0700

    unsatisfying movement in direction of head

---
+++ Assets/UtilityScripts/MoveRoomInheadDirection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRoomInheadDirection : MonoBehaviour
+{	
+	public Transform room;
+	public float speed = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		room.position += Time.deltaTime * speed * transform.forward;
+    }
+}
