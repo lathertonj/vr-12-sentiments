8280fe4c34efc731742a76bf413584fa63dba6c4
Jack Atherton <atherton.lawrence@gmail.com>
2018-10-24 11:50:54 -0700

    two confusing movement paradigms

---
+++ Assets/UtilityScripts/HeadOnlySegwayMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadOnlySegwayMovement : MonoBehaviour
+{
+	public Transform room;
+	public float speed = 5f;
+	private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+		offset = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 movementDirection = transform.localPosition;
+		movementDirection.y = 0;
+		room.position += Time.deltaTime * speed * movementDirection;
+    }
+}
---
+++ Assets/UtilityScripts/SegwayMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegwayMovement : MonoBehaviour
+{
+
+    public Transform room, leftHand, rightHand;
+	public float speed = 5f;
+    private Vector3 initialOffset;
+    // Use this for initialization
+    void Start()
+    {
+		initialOffset = ComputeOffset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 currentOffset = ComputeOffset();
+		Vector3 offsetDifference = currentOffset - initialOffset;
+		offsetDifference.y = 0;
+		room.position += Time.deltaTime * speed * offsetDifference;
+    }
+
+	Vector3 ComputeOffset()
+	{
+		return transform.position - 0.5f * ( leftHand.position + rightHand.position );
+	}
+}
