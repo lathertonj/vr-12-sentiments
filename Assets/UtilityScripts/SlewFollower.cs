9a1b11e79fffcbeddb10b46f8e752eb278f1b7f3
Jack Atherton <atherton.lawrence@gmail.com>
2018-11-08 16:18:56 -0800

    scene 7 trails slightly slewy; not sure if I like it.

---
+++ Assets/UtilityScripts/SlewFollower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlewFollower : MonoBehaviour
+{
+
+    public Transform objectToFollow;
+    public float slew;
+    private Vector3 currentPos, goalPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        // unparent ourselves so we can move on our own
+        transform.parent = null;
+        // set position
+        transform.position = currentPos = goalPos = objectToFollow.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // slew position toward object we're following
+        goalPos = objectToFollow.position;
+        currentPos += Time.deltaTime * slew * ( goalPos - currentPos );
+        transform.position = currentPos;
+    }
+}
