4ff96789626b4f874021315fd84935b849771154
Jack Atherton <atherton.lawrence@gmail.com>
2018-11-14 15:13:35 -0800

    start making interaction happen for scene 8

---
+++ Assets/Objects/Scene8/Scene8SeedlingController.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Scene8SeedlingController : MonoBehaviour
+{
+    public Transform seedlingPrefab;
+    public int numSeedlings = 12;
+    public Vector3 spawnRadius = 1f * Vector3.one;
+    private Rigidbody[] mySeedlings;
+    public ControllerAccessors leftController, rightController;
+    private ParticleSystem leftHand, rightHand;
+    public float maxSqueezeTime = 5f;
+
+    private Scene8SonifySeedlings mySonifier;
+    private ParticleSystem myParticleEmitter;
+    private int numSqueezed = 0;
+
+    public static bool shouldPlayArpeggios = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        myParticleEmitter = GetComponentInChildren<ParticleSystem>();
+        for( int i = 0; i < numSeedlings; i++ )
+        {
+            Transform newSeedling = Instantiate( seedlingPrefab, transform );
+            newSeedling.localPosition = new Vector3(
+                Random.Range( -spawnRadius.x, spawnRadius.x ),
+                Random.Range( -spawnRadius.y, spawnRadius.y ),
+                Random.Range( -spawnRadius.z, spawnRadius.z )
+            );
+
+            newSeedling.localRotation =
+                Quaternion.AngleAxis( Random.Range( -20f, 20f ), Vector3.forward ) *
+                Quaternion.AngleAxis( Random.Range( 5f, 355f ), Vector3.up );
+
+        }
+
+        mySeedlings = GetComponentsInChildren<Rigidbody>();
+        mySonifier = GetComponent<Scene8SonifySeedlings>();
+        mySonifier.StartChuck( jumpDelay: 1.0f, launchASeedling: LaunchASeedling );
+
+        leftHand = leftController.GetComponentInChildren<ParticleSystem>();
+        rightHand = rightController.GetComponentInChildren<ParticleSystem>();
+    }
+
+    void LaunchASeedling()
+    {
+        Rigidbody seedling = mySeedlings[Random.Range( 0, mySeedlings.Length - 1 )];
+        seedling.AddForce( 0.5f * Vector3.up, ForceMode.VelocityChange );
+        Vector3 randomAngularVelocity = new Vector3(
+            Random.Range( -1f, 1f ),
+            Random.Range( -1f, 1f ),
+            Random.Range( -1f, 1f )
+        );
+        seedling.AddTorque( randomAngularVelocity, ForceMode.VelocityChange );
+
+        // animate particle
+        ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
+        emitParams.position = myParticleEmitter.transform.InverseTransformPoint( seedling.transform.TransformPoint( 0.2f * Vector3.up ) );
+        emitParams.velocity = 0.15f * Vector3.up + 0.45f * transform.forward; // match to seedling?
+            //seedling.velocity;//seedling.velocity.y * Vector3.up;  // take only the y velocity?
+        myParticleEmitter.Emit( emitParams, count: 1 );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessControllerInput( leftController, leftHand );
+        ProcessControllerInput( rightController, rightHand );
+    }
+
+    void ProcessControllerInput( ControllerAccessors controller, ParticleSystem hand )
+    {
+        if( controller.IsFirstSqueezed() )
+        {
+            controller.RecordSqueezeStartTime();
+            numSqueezed++;
+            if( numSqueezed == 1 )
+            {
+                mySonifier.InformSqueezed();
+            }
+        }
+
+        if( controller.IsSqueezed() )
+        {
+            float timeElapsed = controller.ElapsedSqueezeTime();
+            
+            // map within low intensity values. this movement is not intense so vibration is not strong.
+            ushort intensity = (ushort) timeElapsed.MapClamp( 0, maxSqueezeTime, 30, 180 );
+            controller.Vibrate( intensity );
+        }
+
+        if( controller.IsUnSqueezed() )
+        {
+            numSqueezed--;
+            if( numSqueezed == 0 )
+            {
+                mySonifier.InformUnsqueezed();
+            }
+
+            float squeezeTime = controller.ElapsedSqueezeTime();
+            Vector3 velocity = controller.Velocity();
+            // x and z are reversed for some reason
+            // ACTUALLY I need to solve this by using the transform, TODO
+            velocity.x *= -1;
+            velocity.z *= -1;
+            Vector3 angularVelocity = controller.AngularVelocity();
+
+            // map time to number of seedlings affected
+            int numSeedlingsToAffect = (int) squeezeTime.MapClamp( 0, maxSqueezeTime, 0, mySeedlings.Length - 0.01f );
+            // pick which ones by traversing in a random order;
+            // the first numSeedlingsToAffect are affected in a primary way
+            // and the remaining seedlings are affected in a secondary way
+            System.Random random = new System.Random();
+            int numSeedlingsProcessed = 0;
+            foreach( int i in Enumerable.Range( 0, mySeedlings.Length ).OrderBy( x => random.Next() ) )
+            {
+                Rigidbody seedling = mySeedlings[i];
+                Vector3 randomAngularVelocity = new Vector3(
+                    Random.Range( -1f, 1f ),
+                    Random.Range( -1f, 1f ),
+                    Random.Range( -1f, 1f )
+                );
+                if( numSeedlingsProcessed < numSeedlingsToAffect )
+                {
+                    // primary action
+                    float randomLargeMultiplier = Random.Range( 0.4f, 0.6f );
+                    seedling.AddForce( randomLargeMultiplier * velocity, ForceMode.VelocityChange );
+                    seedling.AddTorque( angularVelocity * 0.05f + randomAngularVelocity, ForceMode.VelocityChange );
+
+                    // animate for primary action
+                    ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
+                    emitParams.position = myParticleEmitter.transform.InverseTransformPoint( seedling.transform.TransformPoint( 0.2f * Vector3.up ) );
+                    emitParams.velocity = 0.15f * Vector3.up + 0.45f * transform.forward; // match to seedling?
+                        //seedling.velocity;//seedling.velocity.y * Vector3.up;  // take only the y velocity?
+                    myParticleEmitter.Emit( emitParams, count: 1 );
+                }
+                else
+                {
+                    // secondary action
+                    float randomSmallMultiplier = Random.Range( 0.001f, 0.2f );
+                    seedling.AddForce( randomSmallMultiplier * velocity, ForceMode.VelocityChange );
+                    seedling.AddTorque( randomAngularVelocity, ForceMode.VelocityChange );
+                }
+                numSeedlingsProcessed++;
+
+            }
+
+            // sonify arpeggio by num seedlings affected
+            if( shouldPlayArpeggios && numSeedlingsToAffect > 0 )
+            {
+                mySonifier.PlayArpeggio( numSeedlingsToAffect );
+            }
+        }
+    }
+
+}
---
+++ Assets/Sound/Scene8/Scene8SonifySeedlings.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent( typeof( ChuckSubInstance ) )]
+public class Scene8SonifySeedlings : MonoBehaviour
+{
+    private ChuckSubInstance myChuck;
+    public double[] myChord;
+    public double[] myArpeggio;
+
+    private string myChordNotesVar, myArpeggioNotesVar, myModey;
+    private string mySqueezedEvent, myUnsqueezedEvent;
+    private string myJumpEvent;
+    public void StartChuck( float jumpDelay, System.Action launchASeedling )
+    {
+        myChuck = GetComponent<ChuckSubInstance>();
+        myChordNotesVar = myChuck.GetUniqueVariableName();
+        myArpeggioNotesVar = myChuck.GetUniqueVariableName();
+        myModey = myChuck.GetUniqueVariableName();
+        myJumpEvent = myChuck.GetUniqueVariableName();
+        mySqueezedEvent = myChuck.GetUniqueVariableName();
+        myUnsqueezedEvent = myChuck.GetUniqueVariableName();
+
+
+        // respond to individual notes
+        myChuck.RunCode( string.Format( @"
+            global ModalBar {0} => JCRev reverb => dac;
+            global float {1}[1];
+            global Event {2}, {3}, {5};
+            {4}::second => dur noteLength;
+            0.08::second => dur jumpDelay;
+
+            true => int playStrong;
+
+            fun void PlayNotes()
+            {{
+                int i, prevI;
+                while( true )
+                {{
+                    // select next note index
+                    while( i == prevI && {1}.size() > 1 )
+                    {{
+                        Math.random2( 0, {1}.size() - 1 ) => i;
+                        me.yield();
+                    }}
+                    i => prevI;
+
+                    // play note
+                    if( {1}[i] > 10 )
+                    {{
+                        Math.random2f( 0.2, 0.8 ) => {0}.strikePosition;
+                        {1}[i] => Std.mtof => {0}.freq;
+
+                        me.yield();
+
+                        Math.random2f( 0.3, 0.4 ) + playStrong * 0.17 => {0}.strike;
+                        !playStrong => playStrong;
+                        jumpDelay => now;
+                        
+                        // signal a jump should happen
+                        {5}.broadcast();
+                    }}
+                    else
+                    {{
+                        // only wait
+                        jumpDelay => now; 
+                    }}
+
+                    // wait for next note
+                    noteLength - jumpDelay => now;
+                }}
+            }}
+
+            fun void ChangeNoteSpeed()
+            {{
+                jumpDelay + 0.03::second => dur maxSpeed;
+                // actually let's make it longer min speed
+                0.75::second => maxSpeed;
+                while( true )
+                {{
+                    // unsqueezed
+                    {3} => now;
+
+                    // --> make the jumps slow
+                    1.05 *=> noteLength;
+                    if( noteLength > maxSpeed )
+                    {{
+                        maxSpeed => noteLength;
+                        // TODO: send a signal outward?
+                        return;
+                    }}
+                }}
+            }}
+            spork ~ ChangeNoteSpeed();
+
+            fun void RespondToSqueezes() 
+            {{
+                while( true )
+                {{
+                    spork ~ PlayNotes() @=> Shred playNotesShred;
+                    {2} => now; // when squeezed, stop jumping and playing
+                    playNotesShred.exit();
+                    {3} => now;
+                    5::noteLength => now; // wait 3 notes after unsqueezed to resume
+                }}
+            }}
+            spork ~ RespondToSqueezes() @=> Shred squeezeResponseShred;
+
+            global Event scene4EndEvent;
+            scene4EndEvent => now;
+            squeezeResponseShred.exit();
+            
+            // let it die out
+            while( true ) 
+            {{ 
+                {0}.gain() * 0.99 => {0}.gain;
+                10::ms => now;
+            }} 
+
+        ", myModey, myChordNotesVar, mySqueezedEvent, myUnsqueezedEvent, jumpDelay, myJumpEvent ) );
+        myChuck.SetFloatArray( myChordNotesVar, myChord );
+        gameObject.AddComponent<ChuckEventListener>().ListenForEvent( myChuck, myJumpEvent, launchASeedling );
+    }
+
+    public void InformSqueezed()
+    {
+        myChuck.BroadcastEvent( mySqueezedEvent );
+    }
+
+    public void InformUnsqueezed()
+    {
+        myChuck.BroadcastEvent( myUnsqueezedEvent );
+    }
+
+    public float[] PlayArpeggio( int numNotes )
+    {
+        // enforce max to prevent arpeggio from going on too long 
+        numNotes = (int) Mathf.Min( numNotes, 18 );
+        
+        // fill array with PART OF one copy, then random notes
+        string[] newArpeggio = new string[numNotes];
+        float[] toReturn = new float[numNotes];
+        int j = 0;
+        int prevJ = j;
+        for( int i = 0; i < numNotes; i++ )
+        {
+            if( i < myArpeggio.Length / 3 ) // only take first third of array
+            {
+                j = i;
+            }
+            else
+            {
+                while( j == prevJ )
+                {
+                    j = Random.Range( 0, myArpeggio.Length );
+                }
+            }
+            toReturn[i] = (float) myArpeggio[j];
+            newArpeggio[i] = myArpeggio[j].ToString("0.0");
+            prevJ = j;
+        }
+
+        string notes = "[" + string.Join( ", ", newArpeggio ) + "]";
+
+        myChuck.RunCode( string.Format( @"
+            {1} @=> float {0}[]; 
+
+            // make non global so it's not overwriting itself
+            ModalBar {2} => JCRev reverb => dac;
+
+            0.10::second => dur noteLength;
+            true => int hardPick;
+
+            fun void PlayArray()
+            {{
+                for( int i; i < {0}.size(); i++ )
+                {{
+                    if( {0}[i] > 10 )
+                    {{
+                        // strike position
+                        Math.random2f( 0.2, 0.8 ) => {2}.strikePosition;
+                        // freq
+                        {0}[i] => Std.mtof => {2}.freq;
+                        // strike it!
+                        Math.random2f( 0.3, 0.4 ) + 0.17 * hardPick => {2}.strike;
+                        // next pick in opposite direction
+                        !hardPick => hardPick;
+                    }}
+                    else
+                    {{
+                        // next pick in stronger direction
+                        true => hardPick;
+                    }}
+        
+                    noteLength => now;
+                    1.09 *=> noteLength;
+                }}
+            }}
+            PlayArray();
+            5::second => now;
+        ", myArpeggioNotesVar, notes, myModey ) );
+
+        return toReturn;
+    }
+}
