3d15509ea88926d3d61b87ef423a8aa38c00db04
Jack Atherton <atherton.lawrence@gmail.com>
2018-11-15 14:12:01 -0800

    scene 8 fade in

--- Assets/UtilityScripts/FadeInScene.cs
+++ Assets/UtilityScripts/FadeInScene.cs
@@ -8,12 +8,24 @@ public class FadeInScene : MonoBehaviour
     public Color skyColor;
     public float waitTime;
     public float fadeTime;
+    public bool debugFadeInColor = false;
 
     // Use this for initialization
     void Start()
     {
         SteamVR_Fade.Start( skyColor, duration: 0 );
-        Invoke( "DoFade", waitTime );
+        if( !debugFadeInColor )
+        {
+            Invoke( "DoFade", waitTime );
+        }
+    }
+
+    void Update()
+    {
+        if( debugFadeInColor )
+        {
+            SteamVR_Fade.Start( skyColor, duration: 0 );
+        }
     }
 
     void DoFade()
