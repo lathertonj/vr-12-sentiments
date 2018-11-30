e2aeca5dc36f2cb06a28ce6647c8eddbd2d655e8
Jack Atherton <atherton.lawrence@gmail.com>
2018-11-29 16:44:29 -0800

    spawn raindrops

---
+++ Assets/Objects/Scene9/SlowTimeWhenHandsMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTimeWhenHandsMove : MonoBehaviour
+{
+	
+    ControllerAccessors[] controllers;
+    SetTime timeManager;
+    public float minSpeed = 0.05f;
+    public float maxSpeed = 0.5f;
+
+    private float goalTime, currentTime, timeSlew;
+
+    // Use this for initialization
+    void Start()
+    {
+        controllers = transform.parent.GetComponentsInChildren<ControllerAccessors>();
+        timeManager = GetComponent<SetTime>();
+        goalTime = currentTime = 1;
+        timeSlew = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 0;
+        foreach( ControllerAccessors c in controllers ) { speed += c.Velocity().magnitude; }
+        goalTime = speed.PowMapClamp( 0, 10, minSpeed, maxSpeed, pow: 1f );
+        currentTime += timeSlew * ( goalTime - currentTime );
+        timeManager.Scale( currentTime );
+    }
+}
---
+++ Assets/Objects/Scene9/SpawnRaindrops.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRaindrops : MonoBehaviour
+{
+	public Transform raindropPrefab;
+	public Vector3 radius;
+
+    // Use this for initialization
+    void Start()
+    {
+		InvokeRepeating( "SpawnARaindrop", 0, 1f );
+    }
+
+	void SpawnARaindrop()
+	{
+		Vector3 newPosition = new Vector3(
+			Random.Range( -radius.x, radius.x ),
+			Random.Range( -radius.y, radius.y ),
+			Random.Range( -radius.z, radius.z )
+		);
+
+		Instantiate( raindropPrefab, transform.position + newPosition, Quaternion.identity );
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
