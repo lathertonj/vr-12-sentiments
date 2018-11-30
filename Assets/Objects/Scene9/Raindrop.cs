2095ce73e43bb67dc2c2340abe9ebb078b795ccf
Jack Atherton <atherton.lawrence@gmail.com>
2018-11-29 16:16:42 -0800

    jiggling raindrop

---
+++ Assets/Objects/Scene9/Raindrop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raindrop : MonoBehaviour
+{
+
+	private MeshDeformer myDeformer;
+
+    // Use this for initialization
+    void Start()
+    {
+		myDeformer = GetComponentInChildren<MeshDeformer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	public void DeformRaindrop( Vector3 worldPoint, float force )
+	{
+		myDeformer.AddDeformingForce( worldPoint, force );
+	}
+}
---
+++ Assets/Objects/Scene9/RaindropInteractor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaindropInteractor : MonoBehaviour
+{
+    private ControllerAccessors controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<ControllerAccessors>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter( Collider other )
+    {
+        Raindrop maybeRaindrop = other.GetComponentInParent<Raindrop>();
+        if( maybeRaindrop != null )
+        {
+            RaycastHit hit;
+            if( Physics.Raycast( transform.position, maybeRaindrop.transform.position - transform.position, out hit ) )
+            {
+                maybeRaindrop.DeformRaindrop( hit.point, Mathf.Clamp( 2 * controller.Velocity().magnitude, 0, 5 ) );
+            }
+        }
+    }
+
+
+}
