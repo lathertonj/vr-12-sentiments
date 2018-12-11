2399d6f050389efb8b6643b58398f31acf19847f
Jack Atherton <atherton.lawrence@gmail.com>
2018-12-11 14:53:22 -0800

    make sproutlings for scene 11

---
+++ Assets/UtilityScripts/RandomizeScale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeScale : MonoBehaviour
+{
+	public float minScale = 1f;
+	public float maxScale = 1.3f;
+
+    // Use this for initialization
+    void Start()
+    {
+		transform.localScale *= Random.Range( minScale, maxScale );
+    }
+
+    
+}
