164f8a580a81c72fb46effafd64d28074967f153
Jack Atherton <atherton.lawrence@gmail.com>
2019-04-23 16:08:26 -0700

    bug fixes around movement in movement 12

---
+++ Assets/UtilityScripts/Movement/ControllerDirectionMovementScene12.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerDirectionMovementScene12 : MonoBehaviour
+{
+
+    ControllerAccessors controller;
+    ConstantDirectionMover room;
+	public float maxSpeed = 3f;
+    public float slowSpeedCutoff = 1f;
+    public float fastSpeedCutoff = 2f;
+    public Scene12SonifyFlowerSeedlings sonifier;
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<ControllerAccessors>();
+        room = GetComponentInParent<ConstantDirectionMover>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( controller.IsUnSqueezed() )
+        {
+            Vector3 v = controller.Velocity();
+            float speed = v.magnitude.MapClamp( 0, 3, 0, maxSpeed );
+            // transform direction from local space to world space
+            Vector3 direction = room.transform.TransformDirection( v );
+            room.SetDirection( direction.normalized, speed );
+
+            if( speed < slowSpeedCutoff )
+            {
+                sonifier.SlowMovementHappened();
+            }
+            else if( speed > fastSpeedCutoff )
+            {
+                sonifier.FastMovementHappened();
+            }
+        }
+
+    }
+}
