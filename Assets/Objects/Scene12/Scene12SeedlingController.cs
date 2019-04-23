f75ff61538ec4c8a9b9bbf530555450dfc2d224e
Jack Atherton <atherton.lawrence@gmail.com>
2019-04-23 15:44:45 -0700

    seedling arpeggio added, but needs to be tweaked to fit in plot later

---
+++ Assets/Objects/Scene12/Scene12SeedlingController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene12SeedlingController : MonoBehaviour , CollisionResponder
+{
+
+    public Transform seedlingPrefab;
+    public int numSeedlings = 20;
+    public Vector3 spawnRadius = 3f * Vector3.one;
+    private Rigidbody[] mySeedlings;
+    public ControllerAccessors leftController, rightController;
+    private ParticleSystem leftHand, rightHand;
+    private ParticleSystem myParticleEmitter;
+	private ConstantDirectionMover room;
+	private float maxSqueezeTime = 5f;
+    public Transform audioListenerPosition;
+
+
+    private Scene12SonifyFlowerSeedlings mySonifier;
+
+    // Use this for initialization
+    void Start()
+    {
+        myParticleEmitter = GetComponentInChildren<ParticleSystem>();
+		room = GetComponentInParent<ConstantDirectionMover>();
+
+        for( int i = 0; i < numSeedlings; i++ )
+        {
+            Transform newSeedling = Instantiate( seedlingPrefab, transform );
+            newSeedling.localPosition = new Vector3(
+                Random.Range( -spawnRadius.x, spawnRadius.x ),
+                Random.Range( -spawnRadius.y, spawnRadius.y ),
+                Random.Range( -spawnRadius.z, spawnRadius.z )
+            );
+
+            newSeedling.localRotation =
+                Quaternion.AngleAxis( Random.Range( -20f, 20f ), Vector3.forward ) *
+                Quaternion.AngleAxis( Random.Range( 5f, 355f ), Vector3.up );
+
+        }
+
+        mySeedlings = GetComponentsInChildren<Rigidbody>();
+
+        mySonifier = GetComponent<Scene12SonifyFlowerSeedlings>();
+        mySonifier.StartChuck( jumpDelay: 0.4f, launchASeedling: LaunchASeedling, numSeedlings: numSeedlings );
+
+        leftHand = leftController.GetComponentInChildren<ParticleSystem>();
+        rightHand = rightController.GetComponentInChildren<ParticleSystem>();
+
+    }
+
+
+    int currentSeedling = 0;
+    void LaunchASeedling()
+    {
+		Rigidbody seedling = mySeedlings[currentSeedling];
+
+		Vector3 seedlingVelocity = 0.15f * room.GetDirection() + 0.02f * RandomVector3();
+		seedling.AddForce( seedlingVelocity, ForceMode.VelocityChange );
+
+		Vector3 randomAngularVelocity = RandomVector3();
+		seedling.AddTorque( randomAngularVelocity, ForceMode.VelocityChange );
+
+
+		// animate particle
+		ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
+		emitParams.position = myParticleEmitter.transform.InverseTransformPoint( seedling.position );
+		emitParams.velocity = seedlingVelocity; // take only the y component?
+		myParticleEmitter.Emit( emitParams, count: 1 );
+
+        currentSeedling++; currentSeedling %= mySeedlings.Length;
+		
+        // tell chuck what the next distance will be
+        mySonifier.InformOfNextDistance( ( audioListenerPosition.position - mySeedlings[currentSeedling].transform.position ).magnitude );
+    }
+
+    void Update()
+    {
+		ProcessController( leftController );
+        ProcessController( rightController );
+    }
+
+	void ProcessController( ControllerAccessors controller )
+	{
+        if( controller.IsFirstSqueezed() )
+        {
+            controller.RecordSqueezeStartTime();
+        }
+
+		if( controller.IsSqueezed() )
+        {
+            float timeElapsed = controller.ElapsedSqueezeTime();
+            
+            // map within low intensity values. this movement is not intense so vibration is not strong.
+            ushort intensity = (ushort) timeElapsed.MapClamp( 0, maxSqueezeTime, 30, 180 );
+            controller.Vibrate( intensity );
+        }
+	}
+
+	Vector3 RandomVector3()
+	{
+		return new Vector3(
+			Random.Range( -1f, 1f ),
+			Random.Range( -1f, 1f ),
+			Random.Range( -1f, 1f )
+		);
+	}
+
+    public void RespondToCollision()
+    {
+        // when we hit the ground, stop the happiness
+        mySonifier.SlowMovementHappened();
+    }
+}
---
+++ Assets/Sound/Scene12/Scene12SonifyFlowerSeedlings.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene12SonifyFlowerSeedlings : MonoBehaviour {
+
+	private ChuckSubInstance myChuck;
+    public double[] myChord;
+
+    private string myJumpEvent, myIncreaseSpeedEvent, myDecreaseSpeedEvent, myNextLoudness;
+    public void StartChuck( float jumpDelay, System.Action launchASeedling, int numSeedlings )
+    {
+        myChuck = GetComponent<ChuckSubInstance>();
+        myJumpEvent = myChuck.GetUniqueVariableName();
+        myIncreaseSpeedEvent = myChuck.GetUniqueVariableName();
+        myDecreaseSpeedEvent = myChuck.GetUniqueVariableName();
+        myNextLoudness = myChuck.GetUniqueVariableName();
+
+        // copy my chord into longer version that just repeats it
+        string[] expandedChord = new string[numSeedlings];
+        int j = 0;
+        for( int i = 0; i < expandedChord.Length; i++ )
+        {
+            expandedChord[i] = myChord[j].ToString( "0.00" );
+            j++;
+            j %= myChord.Length;
+        }
+
+        string notesString = "[" + string.Join( ", ", expandedChord ) + "]";
+
+        // respond to individual notes
+        myChuck.RunCode( string.Format( @"
+            ModalBar modey => JCRev reverb => dac;
+            {2} @=> float notes[];
+            global Event {1}, {3}, {4};
+            {0}::second => dur noteLength;
+            0.29::second => dur happyModeCutoff;
+            global int scene12HappyMode;
+            false => global int scene12HappyFinishMode;
+            0.05::second => dur jumpDelay;
+            global float {5};
+
+            true => int playStrong;
+
+            fun void PlayNotes()
+            {{
+                while( true )
+                {{
+                    for( int i; i < notes.size(); i++ )
+                    {{
+                        // play note
+                        if( notes[i] > 10 )
+                        {{
+                            Math.random2f( 0.2, 0.8 ) => modey.strikePosition;
+                            if( scene12HappyMode && maybe )
+                            {{
+                                notes[i] + 12 => Std.mtof => modey.freq;
+                            }}
+                            else
+                            {{
+                                notes[i] => Std.mtof => modey.freq;
+                            }}
+
+                            me.yield();
+
+                            Math.random2f( 0.3, 0.4 ) + playStrong * 0.17 => modey.strike;
+                            !playStrong => playStrong;
+
+                            {5} => modey.gain;
+
+                            jumpDelay => now;
+                        
+                            // signal a jump should happen
+                            {1}.broadcast();
+                        }}
+                        else
+                        {{
+                            // only wait
+                            jumpDelay => now; 
+                        }}
+
+                        // wait for next note
+                        noteLength - jumpDelay => now;
+                    }}
+                }}
+            }}
+
+            fun void IncreaseNoteSpeed()
+            {{
+                // min speed
+                0.15::second => dur minSpeed;
+                while( true )
+                {{
+                    // time to increase
+					{3} => now;
+
+                    // --> make the jumps fast
+                    0.94 *=> noteLength;
+                    if( noteLength < minSpeed )
+                    {{
+                        minSpeed => noteLength;
+                    }}
+
+                    if( noteLength < happyModeCutoff )
+                    {{
+                        true => scene12HappyMode;
+                    }}
+                }}
+            }}
+            spork ~ IncreaseNoteSpeed();
+
+            fun void DecreaseNoteSpeed()
+            {{
+                // min speed
+                0.5::second => dur maxSpeed;
+                while( true )
+                {{
+                    // time to decrease
+					{4} => now;
+
+                    // --> make the jumps slow
+                    1.02 *=> noteLength;
+                    if( noteLength > maxSpeed )
+                    {{
+                        maxSpeed => noteLength;
+                    }}
+
+                    // exit happy mode on any slowdown
+                    false => scene12HappyMode;
+                }}
+            }}
+            spork ~ DecreaseNoteSpeed();
+
+            spork ~ PlayNotes() @=> Shred playNotesShred;
+
+
+
+            Shakers s => JCRev rev2 => dac;
+            0.05 => rev2.mix;
+
+            // params
+            0.7 => s.decay; // 0 to 1
+            50 => s.objects; // 0 to 128
+            11 => s.preset; // 0 to 22.  0 is good for galloping. 
+            // 11 is good for eighths
+
+            fun void OnBeats()
+            {{
+                while( true )
+                {{
+                    if( scene12HappyMode || maybe )
+                    {{
+                        Math.random2f( 0.7, 0.9 ) => s.energy;
+                        1 => s.noteOn;
+                    }}
+                    noteLength => now;
+    
+                    if( scene12HappyMode || maybe )
+                    {{
+                        Math.random2f( 0.3, 0.5 ) => s.energy;
+                        1 => s.noteOn;
+                    }}
+                    noteLength => now;
+                }}
+            }}
+            spork ~ OnBeats() @=> Shred playShakersShred;
+
+            fun void ShakersGain()
+            {{
+                float currentGain;
+                0.001 => float upSlew;
+                0.0003 => float downSlew;
+                while( true ) 
+                {{
+                    if( scene12HappyMode > currentGain )
+                    {{
+                        upSlew * ( scene12HappyMode - currentGain ) +=> currentGain;
+                    }}
+                    else
+                    {{
+                        downSlew * ( scene12HappyMode - currentGain ) +=> currentGain;
+                    }}
+                    currentGain * 0.3 => s.gain;
+                    1::ms => now;
+                }}
+            }}
+            spork ~ ShakersGain();
+
+            fun void OverrideHappyMode()
+            {{
+                while( true )
+                {{
+                    if( scene12HappyFinishMode )
+                    {{
+                        true => scene12HappyMode;
+                    }}
+                    1::ms => now;
+                }}
+            }}
+            spork ~ OverrideHappyMode();
+
+
+            global Event scene12Finish;
+            scene12Finish => now; 
+            
+            playNotesShred.exit();
+            playShakersShred.exit();
+
+            // reverb tails
+            10::second => now;
+            
+
+        ", jumpDelay, myJumpEvent, notesString, myIncreaseSpeedEvent, myDecreaseSpeedEvent, myNextLoudness ) );
+        gameObject.AddComponent<ChuckEventListener>().ListenForEvent( myChuck, myJumpEvent, launchASeedling );
+    }
+
+    public void FastMovementHappened()
+    {
+        myChuck.BroadcastEvent( myIncreaseSpeedEvent );
+    }
+
+    public void SlowMovementHappened()
+    {
+        myChuck.BroadcastEvent( myDecreaseSpeedEvent );
+    }
+
+    public void InformOfNextDistance( float distance )
+    {
+        myChuck.SetFloat( myNextLoudness, distance.PowMapClamp( 0, 1, 1, 0.3f, 2 ) );
+    }
+}
